664af911d8d9b932af0ed99f6e81fa328e3d70ce
malnick <malnick@gmail..com>
2015-07-04 19:10:40 -0700

    marathon_template top level class

---
+++ lib/marathon_template.rb
@@ -0,0 +1,34 @@
+#!/bin/env ruby
+
+begin
+
+  require 'rubygems'
+  require 'logger'
+  require 'json'
+  require 'yaml'
+  require 'optparse'
+  require 'fileutils'
+  require 'net/http'
+
+  # Require all my libs
+  library_files = Dir[File.join(File.dirname(__FILE__), "*.rb")].sort
+  library_files.each do |f|
+    require f
+  end
+
+rescue Exception => e
+
+  puts "Failure during requires..."
+  puts e.message
+  puts e.backtrace
+
+end
+
+begin
+  LOG = Logger.new(STDOUT) 
+  config_path = ENV['MARATHON_TEMPLATE_CONFIG_PATH'] || '/etc/haproxy.yaml'
+  CONFIG = MARATHON_TEMPLATE::Options.initialize(config_path)
+rescue Exception => e
+  puts e.backtrace
+  puts e.message
+end
